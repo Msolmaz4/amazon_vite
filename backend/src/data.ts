d08ed060ce30ba48284fd5d33d95f69c597e0bcd
mustafa <msolmaz83@gmail.com>
2023-09-07 00:44:25 +0200

    backend

---
+++ backend/src/data.ts
@@ -0,0 +1,29 @@
+import { Product } from "./types/Product";
+
+export const sampleProducts :Product [] = [
+    {
+        name:'Nikw=e Slim shirt',
+        slug: 'ASlim-shirt',
+        image:'../public/images/p1.jpg',
+        category:'SHIRTS',
+        brand:'NIKE',
+        price:120,
+        countInStock:10,
+        description:'HIGH   quality shirt',
+        rating:4.5,
+        numReviews:10,
+    },
+    {
+        name:'Adidas Slim shirt',
+        slug: 'Nike-Slim-shirt',
+        image:'../public/images/p2.jpg',
+        category:'SHIRTS',
+        brand:'NIKE',
+        price:120,
+        countInStock:10,
+        description:'HIGH   quality shirt',
+        rating:4.5,
+        numReviews:10,
+    },
+
+]
