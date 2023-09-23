0c0c1bf589f04fc2d05bffddbdb517a28c79b91b
mustafa <msolmaz83@gmail.com>
2023-09-23 22:04:59 +0200

    jwt

--- backend/src/data.ts
+++ backend/src/data.ts
@@ -1,4 +1,6 @@
-import { Product } from "./types/Product";
+import { User } from "./models/userModel";
+import { Product } from "./models/productModel";
+import bcrypt from 'bcryptjs'
 
 export const sampleProducts :Product [] = [
     {
@@ -24,30 +26,20 @@ export const sampleProducts :Product [] = [
         description:'HIGH   quality shirt',
         rating:4.5,
         numReviews:10,
-    },
-    {
-        name:'Nikw=e Slim shirt',
-        slug: 'ASlim-shirt',
-        image:'../public/images/p1.jpg',
-        category:'SHIRTS',
-        brand:'NIKE',
-        price:120,
-        countInStock:10,
-        description:'HIGH   quality shirt',
-        rating:4.5,
-        numReviews:10,
-    },
-    {
-        name:'Adidas Slim shirt',
-        slug: 'Nike-Slim-shirt',
-        image:'../public/images/p2.jpg',
-        category:'SHIRTS',
-        brand:'NIKE',
-        price:120,
-        countInStock:10,
-        description:'HIGH   quality shirt',
-        rating:4.5,
-        numReviews:10,
-    },
+    }
 
 ]
+export const sampleUsers: User[] = [
+    {
+      name: 'Joe',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'John',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ]
